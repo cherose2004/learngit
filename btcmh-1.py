62ddace52be36375a9c301b334fc62ae00ecca9f
cherose <cherose@sohu.com>
2019-10-24 16:52:55 +0800

    last modify

--- btcmh-1.py
+++ btcmh-1.py
@@ -1,27 +1,28 @@
-#!/usr/bin/python3
-
 import requests
 from bs4 import BeautifulSoup
 import re
 
+
 def getHTMLText(url):
-    # try:
+    flag = 0
     r = requests.get(url, timeout = 60)
     r.raise_for_status()
     html = r.text
     Soup = BeautifulSoup(html,"lxml")
     tr = Soup.find_all("tr")
+    in_name = input("输入要选择的医生姓名中的一部分：")
+    print("{0:<10}{1:<8}".format("姓名","出诊时间"))
     for td in tr:
         data = td.find_all("td")
-        if len(data)>=5: 
-            # print(data)
-            print(data[0].string,"--->",data[3].string)
-            print("***")
+        if len(data)>=5 :
+            name = str(data[0].string).replace("　","").replace(" ","").strip()
+            if in_name in name:
+                print("{0:<10}".format(name),end="")
+                print("{0}".format(str(data[3].string).strip().replace(" ","")))
+                flag = 1
+    if flag == 0:
+        print("无信息！")
     return None
-
-    # except:
-        # print('something wrong!')
-        # return ""
 
 def main():
     url="http://www.bjzhongyi.com/jzzn/article/7020.html"
